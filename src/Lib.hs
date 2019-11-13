211bc44d2f0c7b8a933225f2ded093b8a37d9ce0
Kaspar Emanuel <kaspar.emanuel@gmail.com>
2019-11-13 15:28:23 +0000

    Switch to git version of language-openscad

--- src/Lib.hs
+++ src/Lib.hs
@@ -6,4 +6,4 @@ import qualified Language.OpenSCAD as OpenSCAD
 import qualified Data.ByteString.Char8 as B
 
 convert :: B.ByteString -> Either String B.ByteString
-convert scad = fmap (B.pack . show) $ OpenSCAD.parseFile scad
+convert scad = fmap (B.pack . show) $ OpenSCAD.parse scad
